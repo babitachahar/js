f38f8a85f5e3f749468db7cdd672e14726e4c8bf
babita <babitachahar50@gmail.com>
2024-11-17 15:29:21 +0000

    contorl flow

---
+++ 03_basic/context.js
@@ -0,0 +1,18 @@
+//An execution context is an abstract concept that represents the environment in which JavaScript code is executed.
+
+/* three types
+Global Execution Context (GEC)
+Function Execution Context (FEC):
+Eval Execution Context:
+*/
+
+
+/*
+Memory phase  creation
+excution phase
+
+*/
+
+/*
+call stack ; first in last out and last in first out 
+*/
